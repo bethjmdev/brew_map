a8d97363ea4526ae5b9be7d7c90b2c6c21a9f1a7
bethjm <b.jeanne.mills@gmail.com>
2024-11-13 13:25:54 -0800

    added root colors

---
+++ src/components/pages/shop/ViewShop.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ViewShop() {
+  return (
+    <div styles={{ backgroundColor: "white", height: "100vh", width: "40rem" }}>
+      <p>hi from</p>
+      <p>View SHop</p>
+    </div>
+  );
+}
+
+export default ViewShop;
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,6 @@
 const colors = {
   latte_brown: "#806D5B",
   espresso_brown: "#4f3e31",
-  //   cream_with_coffee_brown: ,
-  //   opac_white: "rgba(255, 255, 255, 0.7)",
   white: "#ffffff",
   light_grey: "#bdb6b6",
   grey: "#928c8c",
