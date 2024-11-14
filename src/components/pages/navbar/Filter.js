84b41b9287f6600436a56461466cdbd9c34aef6f
bethjm <b.jeanne.mills@gmail.com>
2024-11-14 07:29:16 -0800

    corrected styling for filter to match the other nav bar buttons

--- src/components/pages/navbar/Filter.js
+++ src/components/pages/navbar/Filter.js
@@ -7,40 +7,29 @@ function Filter() {
   return (
     <div
       style={{
-        marginTop: "-20px",
-        padding: "1rem",
-        height: "50vh",
-        position: "fixed",
-        bottom: "0",
-        left: "0",
-        right: "0",
-        // backgroundColor: "white",
+        position: "absolute",
+        top: "0px",
+        right: "3.5rem",
+        backgroundColor: "white",
+        padding: "0.5rem",
+        borderRadius: "0.5rem",
+        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.3)",
+        cursor: "pointer",
+        width: "15rem",
         zIndex: 10,
-        borderTopLeftRadius: "1rem",
-        borderTopRightRadius: "1rem",
-        overflowY: "auto",
       }}
     >
       <div style={{ marginBottom: "1rem" }}>
         <h2>Filter by Roast</h2>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-            gap: "0.5rem",
-            padding: "0.5rem",
-            backgroundColor: "#f9f9f9",
-            borderRadius: "0.5rem",
-          }}
-        >
+        <div>
           {roastOptions.map((roast) => (
             <label
               key={roast}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                padding: "0.25rem",
-              }}
+              // style={{
+              //   display: "flex",
+              //   alignItems: "center",
+              //   padding: "0.25rem",
+              // }}
             >
               <input
                 type="radio"
@@ -54,40 +43,6 @@ function Filter() {
           ))}
         </div>
       </div>
-
-      {/* <div style={{ marginBottom: "1rem" }}>
-        <h2>Filter by Drink</h2>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-            gap: "0.5rem",
-            padding: "0.5rem",
-            backgroundColor: "#f9f9f9",
-            borderRadius: "0.5rem",
-          }}
-        >
-          {drinkOptions.map((drink) => (
-            <label
-              key={drink}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                padding: "0.25rem",
-              }}
-            >
-              <input
-                type="radio"
-                value={drink}
-                checked={selectedDrink === drink}
-                onChange={() => setSelectedDrink(drink)}
-                style={{ marginRight: "0.5rem" }}
-              />
-              {drink}
-            </label>
-          ))}
-        </div>
-      </div> */}
     </div>
   );
 }
