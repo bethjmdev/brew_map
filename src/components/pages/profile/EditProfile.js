ab443a9a859a847437459eb07a9cd631ad26d115
bethjm <b.jeanne.mills@gmail.com>
2024-11-15 14:04:13 -0800

    added styling to edit profile

--- src/components/pages/profile/EditProfile.js
+++ src/components/pages/profile/EditProfile.js
@@ -4,6 +4,8 @@ import { db, auth } from "../../../utils/auth/firebase";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SignUpInfo from "../signup/SignUpInfo";
+import "./EditProfile.css";
+// import Info from "./Info";
 
 const EditProfile = ({ navigate }) => {
   const [profileData, setProfileData] = useState({
@@ -148,76 +150,80 @@ const EditProfile = ({ navigate }) => {
   };
 
   return (
-    <section>
-      <h2>Edit Profile</h2>
-      <ToastContainer position="top-right" />
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            value={profileData.firstName}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            name="lastName"
-            value={profileData.lastName}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={profileData.email}
-            onChange={handleChange}
-            readOnly
-          />
-        </label>
-        <label>
-          Favorite Cafe:
-          <input
-            type="text"
-            name="favCafe"
-            value={profileData.favCafe}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          About:
-          <input
-            type="text"
-            name="about"
-            value={profileData.about}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Your City:
-          <input
-            type="text"
-            name="yourCity"
-            value={profileData.yourCity}
-            onChange={handleChange}
-          />
-        </label>
+    <div className="edit-profile">
+      <div className="edit-profile-container">
+        <h2>Edit Profile</h2>
+        <ToastContainer position="top-right" />
+        <form onSubmit={handleSubmit}>
+          <label>
+            First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={profileData.firstName}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={profileData.lastName}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={profileData.email}
+              onChange={handleChange}
+              readOnly
+            />
+          </label>
+          <br />
+          <label>
+            Favorite Cafe:
+            <input
+              type="text"
+              name="favCafe"
+              value={profileData.favCafe}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            About:
+            <input
+              type="text"
+              name="about"
+              value={profileData.about}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Your City:
+            <input
+              type="text"
+              name="yourCity"
+              value={profileData.yourCity}
+              onChange={handleChange}
+            />
+          </label>
 
-        {/* Pass profileData selections and handleSelectionChange callback */}
-        <SignUpInfo
-          selections={selections}
-          profileData={profileData}
-          onSelectionChange={handleSelectionChange}
-        />
+          {/* Pass profileData selections and handleSelectionChange callback */}
+          <SignUpInfo
+            selections={selections}
+            profileData={profileData}
+            onSelectionChange={handleSelectionChange}
+          />
 
-        <button type="submit">Save Changes</button>
-      </form>
-    </section>
+          <button type="submit">Save Changes</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/pages/profile/Info.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Info = ({ selections }) => (
+  <div>
+    {selections.map(({ label, options, selectedValue, onChange }, index) => (
+      <div
+        key={index}
+        //   style={{ marginBottom: "1rem" }}
+      >
+        <h2>{label}</h2>
+        <div
+        //   style={{
+        //     display: "grid",
+        //     gridTemplateColumns: "repeat(3, 1fr)",
+        //     gap: "0.5rem",
+        //     padding: "0.5rem",
+        //     backgroundColor: "#f9f9f9",
+        //     borderRadius: "0.5rem",
+        //   }}
+        >
+          {options.map((option) => (
+            <label
+              key={option}
+              //   style={{
+              //     display: "flex",
+              //     alignItems: "center",
+              //     padding: "0.25rem",
+              //   }}
+            >
+              <input
+                type="radio"
+                value={option}
+                checked={selectedValue === option}
+                onChange={() => onChange(option)}
+                // style={{ marginRight: "0.5rem" }}
+              />
+              {option}
+            </label>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default Info;
