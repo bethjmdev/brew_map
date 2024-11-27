7379418accb55d1086841e16f79fc9ebc5705459
bethjm <b.jeanne.mills@gmail.com>
2024-11-27 13:12:03 -0800

    test

---
+++ functions/eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+      ignores: ["node_modules/**"],
+    },
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-console": "off", // Example rule, customize as needed
+      },
+    },
+  ];
+  
